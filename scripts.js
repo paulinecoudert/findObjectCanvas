c10bd27fa6368a246a0248ce571195cce39eb7d5
coudert <paulinecoudert@gmail.com>
2021-06-17 17:07:32 +0200

    ckeck le rectangle

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ window.onload = function () {
    
     elements = [];
     elements.push({
+
         colour: 'rgba(0, 0, 255, 0.1)',
         width: 150,
         height: 100,
@@ -39,17 +40,52 @@ window.onload = function () {
     elements.forEach(function (element) {
         context.fillStyle = element.colour;
         context.fillRect(element.left, element.top, element.width, element.height);
+        
     });
+
     // Add event listener for `click` events.
     canvas.addEventListener('click', function (event) {
         var x = event.pageX - elemLeft,
             y = event.pageY - elemTop;
+           
         // Collision detection between clicked offset and element.
         elements.forEach(function (element) {
             if (y > element.top && y < element.top + element.height
                 && x > element.left && x < element.left + element.width) {
-                alert('tu es bien là');
+                    console.log(element);
+                   
+                alert('yes'+ element.top);
             }
+            
         });
     }, false);
+
 };
+
+
+
+//---------------------- Story Points ---------------------------
+
+//Story point state mechanism
+var storyPoints = {};
+
+
+
+//Update the view
+function refreshScenario() {
+  var known = getAllKnownStoryPoints();
+  var element = elements.find(s => {
+  
+     //Validate that we don't have any bypassers
+      for (var i=0; i<=s.bypass.length; i++) {
+        if ( known[s.bypass[i]] ) {
+           return false;
+        }
+     }
+     
+     //Validate that we have all of them
+     return s.required.every(entry => known[entry])
+  })
+  
+  document.querySelector(".js-text").textContent = element.text;
+}
