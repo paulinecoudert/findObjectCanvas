d9ca9c387d6ca071d57207bc695e6d8a48df86b6
coudert <paulinecoudert@gmail.com>
2021-06-23 17:31:11 +0200

    retour reponse

--- scripts.js
+++ scripts.js
@@ -8,13 +8,13 @@ window.onload = function () {
 
     var checkImg = new Image();
     checkImg.src = "check.png";
+   
 
-   var GameScore= 0;
+
 
 //    checkImg.onload = () => { context.drawImage(checkImg, 0, 0, 170, 170, 0, 0, 70,50)}
 
-
-
+var GameScore = 0;
 
     var elements = [];
     elements.push({
@@ -62,52 +62,59 @@ window.onload = function () {
     elements.forEach(function (element) {
         context.fillStyle = element.colour;
         context.fillRect(element.left, element.top, element.width, element.height);
+       
     });
 
     // Add event listener for `click` events.
     canvas.addEventListener('click',function (event) {
         var x = event.pageX - elemLeft,
             y = event.pageY - elemTop;
-          
+
 
 
         // Collision detection between clicked offset and element.
-        elements.forEach(function (element) {
-            if (y > element.top && y < element.top + element.height
-                && x > element.left && x < element.left + element.width) {
-                 
-                 console.log(x,y);
-    
+        elements.forEach(function (element)  {
 
+            if (el = y > element.top && y < element.top + element.height
+                && x > element.left && x < element.left + element.width) {
+
+                 console.log(x,y);
+              
                  if (element.nom === "Perroquet"){
                     drawCoordinates(x=168,y= 20);
-                    score=+2
-                }
+
+                    }
+
                 if (element.nom === "Elephant"){
                     drawCoordinates(x=105,y= 50);
                 }
                 if (element.nom === "Girafe"){
                     drawCoordinates(x=165,y= 80);
-                
+
                 }
                 if (element.nom === "Hippo"){
                     drawCoordinates(x=145,y= 110);
                 }
+
                 if (element.nom === "Lion"){
                     drawCoordinates(x=80,y= 90);
-                  
                 }
-                GameScore++;
-            
-              
-        
-                    console.log(element.nom );
-                //  alert('lol');
-                document.querySelector(".js-text").textContent = "Tu as trouvé: " + element.nom+' '  + GameScore +"/5 animaux";
                 
+                        if(GameScore <= 4 ){
+                            GameScore++;
+                            document.querySelector(".js-text").textContent = "Tu as trouvé: " + element.nom+' '  + GameScore +"/5 animaux";
+                        };
+
+                        if(GameScore === 5 && !el ){
+                        console.log(el);
+                        document.querySelector(".js-text").textContent = " Bravo le dernier etait: "+ element.nom+' ' +GameScore +"/5 animaux";
+                    
+                        };
             };
-        
+         
+
         });
+    
     }, false);
 
 };
@@ -121,5 +128,7 @@ function drawCoordinates(x,y){
 
     var checkImg = new Image();
     checkImg.src = "check.png";
-    checkImg.onload = () => { context.drawImage(checkImg, x, y, 20, 20)}
+    checkImg.onload = () => { context.drawImage(checkImg, x, y, 20, 15)}
 }
+
+
